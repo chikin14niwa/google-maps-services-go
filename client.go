9b28d2c7d14de7ab5b7da2e6e77df36c34e8d93a
Hayato Nakamura <hyt.rugby@gmail.com>
2019-08-20 09:06:54 +0900

    ローカルビルドするため、内部パッケージへの参照方法を変更

--- client.go
+++ client.go
@@ -25,8 +25,8 @@ import (
 	"net/http"
 	"net/url"
 
+	"github.com/chikin14niwa/google-maps-services-go/internal"
 	"golang.org/x/time/rate"
-	"googlemaps.github.io/maps/internal"
 )
 
 // Client may be used to make requests to the Google Maps WebService APIs
--- encoding.go
+++ encoding.go
@@ -21,7 +21,7 @@ import (
 	"encoding/json"
 	"net/url"
 
-	"googlemaps.github.io/maps/internal"
+	"github.com/chikin14niwa/google-maps-services-go/internal"
 )
 
 // safeLeg is a raw version of Leg that does not have custom encoding or
